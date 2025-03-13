1e75040868c48fa76973ff3a1ceb25a05e6362ee
Моя Прелесть) < )@HOME-PC>
2025-03-13 23:49:33 +0800

    changes

--- project/project.cpp
+++ project/project.cpp
@@ -1,63 +1,82 @@
 ﻿// Чернякова Валерия.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
 //
 #define GLEW_DLL
-#define GLEW_DLL
+#define GLFW_DLL
 
-#include "GL\glew.h"
-#include "GLFW\glfw3.h"
-#include "cstdio"
-
+#include <cstdio>
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
 #include <iostream>
+#include "ShaderLib.h"
+
+GLfloat points[] = {
+    -0.5,  0.5, 0.0,
+     0.5,  0.5, 0.0,
+    -0.8, -0.5, 0.0,
+     0.8, -0.5, 0.0
+};
+
+
+GLuint index[] = { 0, 1, 2, 1, 2, 3 };
 
 int main()
 {
-    printf  ("Hello World!\n");
-
     glfwInit();
 
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 1);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
-    // glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-    // glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
     GLFWwindow* window;
-     
-    window = glfwCreateWindow(512, 512, "Mainwindow", NULL, NULL);
-
+    window = glfwCreateWindow(512, 512, "lab2", NULL, NULL);
     glfwMakeContextCurrent(window);
-
     glewExperimental = GL_TRUE;
-
     glewInit();
+
+    GLuint VAO, VBO, EBO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
+
+    glBindVertexArray(VAO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+
+    glBufferData(GL_ARRAY_BUFFER, sizeof(points), points, GL_STATIC_DRAW);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(index), index, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GL_FLOAT), 0);
+    glEnableVertexAttribArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+
+    Shader ourShader("vertex_shader.glsl", "fragment_shader.glsl");
 
     while (!glfwWindowShouldClose(window))
     {
-        glClearColor(1.0, 1.0, 0.3, 1.0);
 
+        glClearColor(1.0, 1.0, 0.3, 1.0);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glColor3f(0.5, 0.5, 1.0);
 
-        glBegin(GL_QUADS);
+        float timeValue = glfwGetTime();
 
-        glVertex2f(-0.5, 0.0);
+        float Red = abs(sin(timeValue));
+        float Green = abs(cos(timeValue));
+        float Blue = abs(sin(timeValue * 0.5));
 
-        glVertex2f(0.5, 0.0);
+        ourShader.Use();
 
-        glVertex2f(0.3, 0.5);
+        ourShader.SetUniform("ourColour", Red, Green, Blue, 1.0);
 
-        glVertex2f(-0.3, 0.5);
+        glBindVertexArray(VAO);
 
-        glEnd();
-
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
         glfwSwapBuffers(window);
-
         glfwPollEvents();
-
     }
-
-glfwTerminate();
-
+    glfwTerminate();
 }
 
 // Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
